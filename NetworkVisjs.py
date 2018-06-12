93d6a6816151d9da43f8cbfead39241796897be6
Abderahmen <masmoudi.rhouma@gmail.com>
2018-06-12 12:09:29 +0200

    1st

---
+++ NetworkVisjs.py
@@ -0,0 +1,109 @@
+from __future__ import division
+
+import numpy as np
+
+from bokeh.core.properties import Instance, String, Int
+from bokeh.models import ColumnDataSource, LayoutDOM
+from bokeh.io import show
+
+JS_CODE = """
+import * as p from "core/properties"
+import { LayoutDOM, LayoutDOMView } from "models/layouts/layout_dom"
+OPTIONS =
+    interaction: {
+        hover: true,
+        selectConnectedEdges: true,
+        hoverConnectedEdges: true
+    }
+    manipulation: {
+        enabled: false
+    }
+    nodes: {
+        shape: 'dot',
+        size: 20
+    }
+    edges: {
+        color: {
+            inherit: "both"
+        }
+    }
+
+export class NetworkView extends LayoutDOMView
+
+    initialize: (options) ->
+        super(options)
+
+        url = "https://cdnjs.cloudflare.com/ajax/libs/vis/4.21.0/vis.min.js"
+
+        script = document.createElement('script')
+        script.src = url
+        script.async = false
+        script.onreadystatechange = script.onload = () => @_init()
+        document.querySelector("head").appendChild(script)
+
+    _init: () ->
+        nodes = new vis.DataSet();
+        edges = new vis.DataSet();
+        data = {
+            nodes: nodes,
+            edges: edges
+        };
+        @get_data(nodes,edges);
+        @_graph = new vis.Network(@el, data, OPTIONS)
+        @connect(@model.data_source.change, () => @get_data(nodes,edges))
+        @_graph.on("doubleClick", (params) => @fun(params));
+
+    get_data: (nodes, edges) ->
+        nodes.clear()
+        edges.clear()
+        source = @model.data_source
+        for i in [0...source.get_length()]
+            nodes.add({
+                id: i,
+                label: source.get_column(@model.label)[i],
+                color: source.get_column(@model.color)[i]
+            })
+            for j in [0...source.get_column(@model.edges)[i].length]
+                edges.add({
+                    from: i,
+                    to: source.get_column(@model.edges)[i][j]-1,
+                    title: String((source.get_column(@model.values)[i][j]*10).toFixed 2),
+                    value: (source.get_column(@model.values)[i][j]*10).toFixed 2
+                })
+    
+    fun: (params) ->
+        if params.nodes
+            @model.selected = params.nodes[0]
+    
+        
+
+export class Network extends LayoutDOM
+
+    default_view: NetworkView
+
+    type: "Network"
+
+    @define {
+        label: [p.String]
+        edges: [p.String]
+        values: [p.String]
+        color: [p.String]
+        data_source: [p.Instance]
+        selected: [p.Number]
+    }
+"""
+
+# This custom extension model will have a DOM view that should layout-able in
+# Bokeh layouts, so use ``LayoutDOM`` as the base class. If you wanted to create
+# a custom tool, you could inherit from ``Tool``, or from ``Glyph`` if you
+# wanted to create a custom glyph, etc.
+
+
+class Network(LayoutDOM):
+    __implementation__ = JS_CODE
+    data_source = Instance(ColumnDataSource)
+    label = String
+    edges = String
+    values = String
+    color = String
+    selected = Int(default=-1)
---
+++ main.py
@@ -0,0 +1,431 @@
+import io
+from gensim.models import FastText
+from NetworkVisjs import Network
+from time import time
+import numpy as np
+from numpy import linalg
+from numpy.linalg import norm
+from scipy.spatial.distance import squareform, pdist
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+import sklearn
+import random
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from sklearn.metrics.pairwise import cosine_similarity as cosSim
+from bokeh.io import curdoc
+from bokeh.plotting import figure, show, output_file, ColumnDataSource
+from bokeh.models import CustomJS, Slider, ColumnDataSource, WidgetBox, HoverTool, TapTool, Div, CDSView, GroupFilter, Selection, LinearColorMapper, Circle, ColorBar
+from bokeh.layouts import layout, column, row, Spacer, widgetbox
+from bokeh.models.widgets import Button, AutocompleteInput, TextInput, Panel, Tabs
+from bokeh.models.callbacks import CustomJS
+from bokeh.models.renderers import GlyphRenderer
+from bokeh.palettes import Plasma256
+from bokeh.server.server import BaseServer
+from bokeh.events import SelectionGeometry
+
+nltk.download('punkt')
+
+def load_vectors(fname):
+    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    n, d = map(int, fin.readline().split())
+    data = {}
+    for line in fin:
+        tokens = line.rstrip().split(' ')
+        data[tokens[0]] = map(float, tokens[1:])
+    return data
+
+# Storing each iteration for T-SNE
+positions = []
+X_iter = []
+def _gradient_descent(objective, p0, it, n_iter,
+                      n_iter_check=1, n_iter_without_progress=300,
+                      momentum=0.8, learning_rate=200.0, min_gain=0.01,
+                      min_grad_norm=1e-7, verbose=0, args=None, kwargs=None):
+    if args is None:
+        args = []
+    if kwargs is None:
+        kwargs = {}
+
+    p = p0.copy().ravel()
+    update = np.zeros_like(p)
+    gains = np.ones_like(p)
+    error = np.finfo(np.float).max
+    best_error = np.finfo(np.float).max
+    best_iter = i = it
+
+    tic = time()
+    for i in range(it, n_iter):
+        positions.append(p.copy())
+
+        error, grad = objective(p, *args, **kwargs)
+        grad_norm = linalg.norm(grad)
+
+        inc = update * grad < 0.0
+        dec = np.invert(inc)
+        gains[inc] += 0.2
+        gains[dec] *= 0.8
+        np.clip(gains, min_gain, np.inf, out=gains)
+        grad *= gains
+        update = momentum * update - learning_rate * grad
+        p += update
+
+        if (i + 1) % n_iter_check == 0:
+            toc = time()
+            duration = toc - tic
+            tic = toc
+
+            if verbose >= 2:
+                print("[t-SNE] Iteration %d: error = %.7f,"
+                      " gradient norm = %.7f"
+                      " (%s iterations in %0.3fs)"
+                      % (i + 1, error, grad_norm, n_iter_check, duration))
+
+            if error < best_error:
+                best_error = error
+                best_iter = i
+            elif i - best_iter > n_iter_without_progress:
+                if verbose >= 2:
+                    print("[t-SNE] Iteration %d: did not make any progress "
+                          "during the last %d episodes. Finished."
+                          % (i + 1, n_iter_without_progress))
+                break
+            if grad_norm <= min_grad_norm:
+                if verbose >= 2:
+                    print("[t-SNE] Iteration %d: gradient norm %f. Finished."
+                          % (i + 1, grad_norm))
+                break
+
+    return p, error, i
+sklearn.manifold.t_sne._gradient_descent = _gradient_descent
+
+def to_lowercase(sentences):
+    """Convert all characters to lowercase from list of tokenized words"""
+    new_sentences = []
+    for sentence in sentences:
+        new_words = []
+        for word in sentence:
+            new_word = word.lower()
+            new_words.append(new_word)
+        new_sentences.append(new_words)
+    return new_sentences
+
+print("Starting execution ...")
+#data = load_vectors("PRe/vectors/newWiki300.vec")
+## Training data with fasttext
+# model = fasttext.skipgram('PRe/text/wikipediaTXT.txt','model',bucket=50000)
+'''
+f = open("PRe_new/text/text.txt", encoding='ISO-8859-1')
+txt = f.read()
+List = [word_tokenize(t) for t in sent_tokenize(txt)]
+List = to_lowercase(List)
+model = FastText(List, sg=1, size=300, workers=4, min_count=1)
+'''
+model = FastText.load_fasttext_format('PRe_new/model/model.bin', encoding='ISO-8859-1')
+## model = fasttext.load_model('PRe/model/model.bin')
+##
+print("Data loaded ...")
+# vectors = [list(line) for line in data.values()]
+# words = list(data)
+number_of_elements = 1000
+vectors = model.wv.syn0
+words = model.wv.index2word
+# del model
+# PCA Test
+pca = PCA(n_components=2)
+pca.fit(vectors[0:number_of_elements])
+# print(pca.explained_variance_ratio_)
+transform = pca.transform(vectors[0:number_of_elements])
+print("PCA done ...")
+
+# Representation with Bokeh
+source = ColumnDataSource(data=dict(
+    x=transform[:, 0],
+    y=transform[:, 1],
+    mots=words[0:number_of_elements],
+    color=['#053061' for i in range(0,number_of_elements)],
+))
+sourceTSNE = ColumnDataSource(data=dict(
+    x=[0 for i in range(0,number_of_elements)],
+    y=[0 for i in range(0,number_of_elements)],
+    mots=words[0:number_of_elements],
+    color=['#053061' for i in range(0,number_of_elements)],
+))
+sour = ColumnDataSource(data=dict(
+    label=['one','two','three'],
+    edges=[[2,3],[1],[3]],
+    values=[[0.5,0.2],[0.3],[0.78]],
+    index=[0,1,2],
+    color=['red', 'blue', 'red']
+))
+print('source done ...')
+TOOLS = "pan,wheel_zoom,zoom_in,zoom_out,box_zoom,reset,tap,save".split(
+    ',')
+hover = HoverTool(tooltips=[
+    ("Indice", "$index"),
+    ("Mot", "@mots"),
+])
+TOOLS.append(hover)
+Cpalette = ['#E72018','#E17317','#DCC317','#9FD716','#4CD216','#15CC2D','#15C776','#14C2BC','#147CBD','#1434B8']
+lcm = LinearColorMapper(palette=Cpalette, low=0, high=1)
+color_bar_p = ColorBar(color_mapper=lcm, location=(0, 0))
+color_bar_p2 = ColorBar(color_mapper=lcm, location=(0, 0))
+selected_circle = Circle(fill_alpha=1, fill_color={'field' : 'color', 'transform':lcm}, line_color={'field' : 'color', 'transform':lcm})
+
+p = figure(plot_height=400, tools=TOOLS, output_backend="webgl", active_scroll='wheel_zoom')
+p_circle = p.circle('x', 'y', source=source, color='#053061', fill_alpha=0.6)
+p_circle.selection_glyph = selected_circle
+p.add_layout(color_bar_p, 'left')
+tab1 = Panel(child=p, title="PCA")
+
+p2 = figure(plot_height=400, x_range=(-50, 50), y_range=(-50, 50), tools=TOOLS, output_backend="webgl", active_scroll='wheel_zoom')
+p2_circle = p2.circle('x', 'y', source=sourceTSNE, color='#053061', fill_alpha=0.6)
+p2_circle.selection_glyph = selected_circle
+p2.add_layout(color_bar_p2, 'left')
+tsnePerplexity = Slider(start=5, end=100, value=30, step=1, width=120, title="Perplexity")
+tsneLearning = Slider(start=10, end=1000, value=200, step=1, width=120, title="Learning Rate")
+tsneIteration = Slider(start=300, end=5000, value=500, step=50, width=120, title="Iterations")
+tsneSpeed = Slider(start=10, end=100, value=70, step=1, width=120, title="Speed")
+tsneApply = Button(label='Apply', button_type='success', width=80)
+pauseB = Button(label='Pause', button_type='success', width=60)
+startB = Button(label='Start', button_type='success', width=60)
+stopB = Button(label='Stop', button_type='success', width=60)
+tsneLoading = Div()
+iterationCount = Div()
+resultText = Div()
+minus = Div(text='-',width=15)
+plus = Div(text='+',width=15)
+word1 = TextInput(width=200, title="Analogy")
+word2 = TextInput(width=200, title="-")
+word3 = TextInput(width=200, title="+")
+calculateAnalogy = Button(label='Equals', button_type='success', width=60)
+equals = Div(text=" ", width=120)
+equals.css_classes = ["center"]
+analogy = column(word1,word2,word3,row(calculateAnalogy,Spacer(width=20),equals))
+tsneLayout = layout([
+    [p2, analogy, Spacer(width=25), widgetbox(resultText, width=200)],
+    [tsnePerplexity, Spacer(width=20), tsneLearning, Spacer(width=20), tsneIteration, Spacer(width=20), tsneApply, Spacer(width=20), widgetbox(tsneLoading, width=30)],
+    [tsneSpeed, Spacer(width=20), pauseB, Spacer(width=10), startB, Spacer(width=10), stopB, Spacer(width=20), iterationCount],
+])
+tab2 = Panel(child=tsneLayout, title="t-SNE")
+renderer = p2.select(dict(type=GlyphRenderer))
+ds = renderer[0].data_source
+
+p3 = Network(label="label", edges="edges", values="values", color="color", data_source=sour, width=900, height=550)
+tab3 = Panel(child=p3,title="Network")
+tabs = Tabs(tabs=[tab1, tab2, tab3])
+'''
+#testing analogies
+vec = [x1-x2+x3 for x1,x2,x3 in zip(vectors[563],vectors[313],vectors[513])]
+analog = [cosSim(np.asarray([vec]), np.asarray([b]) )[0][0] for b in vectors]
+analogyList = list(zip([i for i in range(0,len(vectors)-1)], analog))
+sortedAna = sorted(analogyList,key=lambda l:l[1], reverse=True)
+print(words[563], words[313], words[513])
+for i in range(1,10):
+    print(words[sortedAna[i][0]])
+'''
+# Neighbors
+number_of_neighbors = 10
+#allSimilarity = cosSim(vectors)
+
+'''
+def neighbors(source=source, div=resultText, vectors=vectors[0:number_of_elements], cosSim=cosSim):
+    word = source.data['mots'][cb_data.source.selected.indices[0]]
+    wordIndex = cb_data.source.selected.indices[0]
+    print(cb_data)
+    v = [cosSim( np.asarray([vectors[wordIndex]]), np.asarray([b]) )[0][0] for b in vectors]
+    #similarityList = [[i,cosSim(vectors[wordIndex],vectors[i])] for i in range(0,len(vectors)) if i!=wordIndex]
+    similarityList = list(zip([i for i in range(0, len(vectors)-1)], v))
+    sortedSim = sorted(similarityList, key=lambda l:l[1], reverse=True)
+    similarities = "10 neighbors of " + word + " : <br/>"
+    for i in range(1, 10):
+        cb_data.source.selected.indices[i] = sortedSim[i][0]
+        similarities = similarities + \
+            words[sortedSim[i][0]] + " : " + \
+            "{0:.2f}".format(sortedSim[i][1]) + "<br/>"
+    div.text = similarities
+
+# Selection Event
+taptool = p.select(type=TapTool)
+taptool.callback = CustomJS.from_py_func(neighbors)
+
+taptool_TSNE = p2.select(type=TapTool)
+taptool_TSNE.callback = CustomJS.from_py_func(neighbors)
+'''
+def generateColor():
+    color = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+    return color
+def handlerTSNE(attr, old, new, vectors=vectors[0:number_of_elements]):
+    if (len(new.indices) != 0) and (handlerTSNE.update == False) :
+        handlerTSNE.update = True
+        wordIndex = new.indices[0]
+        v = [cosSim( np.asarray([vectors[wordIndex]]), np.asarray([b]) )[0][0] for b in vectors]
+        similarityList = list(zip([i for i in range(0, len(vectors)-1)], v))
+        p2_circle.data_source.add(data=v,name='color')
+        sortedSim = sorted(similarityList, key=lambda l:l[1], reverse=True)
+        similarities = str(number_of_neighbors)+" neighbors of " + words[wordIndex] + " : <br/>"
+        l = [new.indices[0]]
+        sour.data['label'] = [words[wordIndex]]
+        sour.data['edges'] = [[]]
+        sour.data['values'] = [[]]
+        sour.data['index'] = [wordIndex]
+        sour.data['color'] = [generateColor()]
+        color = generateColor()
+        for i in range(1, number_of_neighbors):
+            l.append(sortedSim[i][0])
+            similarities = similarities + words[sortedSim[i][0]] + " : " + "{0:.2f}".format(sortedSim[i][1]) + "<br/>"
+            sour.data['label'].append(words[sortedSim[i][0]])
+            sour.data['edges'].append([1])
+            sour.data['values'].append([sortedSim[i][1]])
+            sour.data['index'].append(sortedSim[i][0])
+            sour.data['color'].append(color)
+        p2_circle.data_source.selected.indices = l
+        p2_circle.data_source.trigger('selected',None,p2_circle.data_source.selected)
+        resultText.text = similarities
+        print(model.wv.most_similar(words[wordIndex]))
+        sour.trigger('data', None, sour)
+        handlerTSNE.update = False
+
+def handler(attr, old, new, vectors=vectors[0:number_of_elements]):
+    if (len(new.indices) != 0) and (handler.update == False) :
+        handler.update = True
+        wordIndex = new.indices[0]
+        v = [cosSim( np.asarray([vectors[wordIndex]]), np.asarray([b]) )[0][0] for b in vectors]
+        similarityList = list(zip([i for i in range(0, len(vectors)-1)], v))
+        p_circle.data_source.add(data=v,name='color')
+        sortedSim = sorted(similarityList, key=lambda l:l[1], reverse=True)
+        sortedSim = np.asarray([list(i) for i in sortedSim])
+        similarities = str(number_of_neighbors)+" neighbors of " + words[wordIndex] + " : <br/>"
+        l = [new.indices[0]]
+        for i in range(1, number_of_neighbors):
+            l.append(int(sortedSim[i][0]))
+            similarities = similarities + words[int(sortedSim[i][0])] + " : " + "{0:.5f}".format(sortedSim[i][1]) + "<br/>"
+        p_circle.data_source.selected.indices = l
+        p_circle.data_source.trigger('selected',None,p_circle.data_source.selected)
+        resultText.text = similarities
+        print(model.wv.most_similar(words[wordIndex]))
+        handler.update = False
+        
+handlerTSNE.update = False
+handler.update = False
+p2_circle.data_source.on_change('selected',handlerTSNE)
+p_circle.data_source.on_change('selected',handler)
+
+scode = """
+    mots = %s;
+    console.log(mots[cb_data.source.selected.indices[0]]);
+    """ % (words)
+#taptool.callback = CustomJS(args=dict(source=source),code = scode)
+
+# T-SNE representation Function
+def tsneProcess(source=ds, vectors=vectors):
+    global positions
+    global X_iter
+    positions = []
+    tsneLoading.css_classes = ["loader"]
+    TSNE_transform = TSNE(n_components=2, n_iter=tsneIteration.value, perplexity=tsnePerplexity.value, learning_rate=tsneLearning.value).fit_transform(vectors[0:number_of_elements])
+    print("TSNE done ...")
+    X_iter = np.dstack(position.reshape(-1, 2) for position in positions)
+    print("X_iter done ...")
+    ds.data['x'] = TSNE_transform[:, 0]
+    ds.data['y'] = TSNE_transform[:, 1]
+    ds.trigger('data',ds.data,ds.data)
+    tsneLoading.css_classes = []
+
+#T-SNE Animation Function
+iteration = 0
+def tsne_animation(length=number_of_elements, div=iterationCount):
+    global ds
+    global iteration
+    global X_iter
+    if iteration < X_iter.shape[2]:
+        for i in range(0,length):
+            ds.data['x'][i] = X_iter[i][0][iteration]
+            ds.data['y'][i] = X_iter[i][1][iteration]
+        
+        ds.trigger('data',ds.data,ds.data)
+        iteration += 1
+        div.text = "Iteration N&#176 : " + str(iteration)
+    else:
+        stopAnimation()
+
+numberElement = TextInput(width=200, placeholder="Nombre d'iterations")
+changeNumberB = Button(label='Change')
+
+# output_file("Vector representation.html", title="Example of vector representation")
+l2 = layout([
+    [numberElement],
+], sizing_mode='scale_width')
+l = layout([
+  [tabs],
+], sizing_mode='scale_width')
+curdoc().add_root(l)
+
+animation = None
+def animate():
+    global animation
+    if (animation == None):
+        animation = curdoc().add_periodic_callback(tsne_animation,110-tsneSpeed.value)
+def destroyAnimation():
+    global animation
+    if (animation != None):
+        curdoc().remove_periodic_callback(animation)
+        animation = None
+        startB.label = "Resume"
+def stopAnimation():
+    global animation
+    global iteration
+    if (animation != None):
+        curdoc().remove_periodic_callback(animation)
+        animation = None
+        iteration = 0
+        startB.label = "Start"
+def changeSpeed(attr, old, new):
+    global animation
+    if (animation != None):
+        curdoc().remove_periodic_callback(animation)
+        animation = None
+        animation = curdoc().add_periodic_callback(tsne_animation,int(110-new))
+def tabChange(attr, old, new):
+    if(new == 1) and (tabChange.first == False):
+        tabChange.first = True
+        tsneProcess()
+tabChange.first = False
+
+def addNeighborNodes(index, vectors=vectors[0:number_of_elements]):
+    wordIndex = sour.data['index'][index]
+    v = [cosSim( np.asarray([vectors[wordIndex]]), np.asarray([b]) )[0][0] for b in vectors]
+    similarityList = list(zip([i for i in range(0, len(vectors)-1)], v))
+    sortedSim = sorted(similarityList, key=lambda l:l[1], reverse=True)
+    color = generateColor()
+    for i in range(1, number_of_neighbors):
+        print(sortedSim[i][0], sour.data['index'], sour.data['edges'][index])
+        if (sortedSim[i][0] in sour.data['index']) and (sour.data['index'].index(sortedSim[i][0])+1 not in sour.data['edges'][index]):
+            indice = sour.data['index'].index(sortedSim[i][0])
+            sour.data['edges'][indice].append(index+1)
+            sour.data['values'][indice].append(sortedSim[i][1])
+        elif (sortedSim[i][0] not in sour.data['index']):
+            sour.data['label'].append(words[sortedSim[i][0]])
+            sour.data['edges'].append([index+1])
+            sour.data['values'].append([sortedSim[i][1]])
+            sour.data['index'].append(sortedSim[i][0])
+            sour.data['color'].append(color)
+    sour.trigger('data', None, sour)
+
+def selectNode(attr, old, new):
+    print(new)
+    addNeighborNodes(new)
+
+def calcAnalogy():
+    word = model.wv.most_similar(positive=[word1.value,word3.value], negative=[word2.value])
+    print(word)
+    equals.text = "<b> <center>"+word[0][0]+"</center> </b>"
+
+pauseB.on_click(destroyAnimation)
+startB.on_click(animate)
+stopB.on_click(stopAnimation)
+tsneSpeed.on_change('value', changeSpeed)
+tabs.on_change('active', tabChange)
+tsneApply.on_click(tsneProcess)
+p3.on_change('selected',selectNode)
+calculateAnalogy.on_click(calcAnalogy)
